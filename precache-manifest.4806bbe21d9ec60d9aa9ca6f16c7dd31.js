04df6c4303461c669ad1c88b43d0f1dda68c5e81
Sktro <yissssss@yandex.ru>
2021-12-13 22:47:50 +0300

    Updates

---
+++ precache-manifest.4806bbe21d9ec60d9aa9ca6f16c7dd31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94f82fc8570b4461ace9f9518c7bddd",
+    "url": "/home-tasks/index.html"
+  },
+  {
+    "revision": "173101ce97b904fa2bca",
+    "url": "/home-tasks/static/css/main.cd46a522.chunk.css"
+  },
+  {
+    "revision": "3208d300034e6d2e7600",
+    "url": "/home-tasks/static/js/2.42313c12.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/home-tasks/static/js/2.42313c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173101ce97b904fa2bca",
+    "url": "/home-tasks/static/js/main.460eacce.chunk.js"
+  },
+  {
+    "revision": "08e17a2123cdaee402e1",
+    "url": "/home-tasks/static/js/runtime-main.e2248177.js"
+  }
+]);
